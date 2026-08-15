e6a3bf7457c205bd066477f19a02349006d18334
Simon Coulton <simon.coulton@gmail.com>
2026-08-15 21:14:58 +1000

    chore: tweak HDR

--- config/hypr/.config/hypr/monitors.lua
+++ config/hypr/.config/hypr/monitors.lua
@@ -1,1 +1,11 @@
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto", bitdepth = 10, cm = "hdr" })
+hl.monitor({
+	output = "",
+	mode = "preferred",
+	position = "auto",
+	scale = "auto",
+	bitdepth = 10,
+	cm = "hdr",
+	sdr_eotf = "gamma22force",
+	sdrbrightness = 1.2,
+	sdrsaturation = 1.0,
+})
